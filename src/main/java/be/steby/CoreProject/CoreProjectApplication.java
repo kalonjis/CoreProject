f04a28abdaff7ee79f205a89e25c1f969f98f163
kalonjis <kalonjistephane@gmail.com>
2025-02-17 10:32:46 +0100

    initial commit

---
+++ src/main/java/be/steby/CoreProject/CoreProjectApplication.java
@@ -0,0 +1,13 @@
+package be.steby.CoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/steby/CoreProject/CoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.steby.CoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
