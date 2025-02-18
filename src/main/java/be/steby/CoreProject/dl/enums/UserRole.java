ca2005bb69cbb5735bba960da0242a8ad593e572
kalonjis <kalonjistephane@gmail.com>
2025-02-18 17:10:37 +0100

    added static method "setRoles"

--- src/main/java/be/steby/CoreProject/dl/enums/UserRole.java
+++ src/main/java/be/steby/CoreProject/dl/enums/UserRole.java
@@ -1,9 +1,22 @@
 package be.steby.CoreProject.dl.enums;
 
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum UserRole {
     SUPER_ADMIN,
     ADMIN,
     MODERATOR,
     USER,
-    SPECTATOR;
+    GUEST;
+
+    public static Set<UserRole> setRoles(UserRole topRole) {
+        return Arrays.stream(UserRole.values())
+                .filter(role -> role.ordinal() >= topRole.ordinal())
+                .collect(Collectors.toSet());
+    }
+
 }
+
