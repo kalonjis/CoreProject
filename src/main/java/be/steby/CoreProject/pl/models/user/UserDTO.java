a53ce7b5174034162372db3b52f3e9c6b1dee747
kalonjis <kalonjistephane@gmail.com>
2025-02-19 14:21:32 +0100

    UserDTO creation

---
+++ src/main/java/be/steby/CoreProject/pl/models/user/UserDTO.java
@@ -0,0 +1,28 @@
+package be.steby.CoreProject.pl.models.user;
+
+import be.steby.CoreProject.dl.entities.User;
+import be.steby.CoreProject.dl.enums.UserRole;
+
+import java.util.Set;
+
+public record UserDTO(
+        Long Id,
+        String username,
+        String firstname,
+        String lastname,
+        String email,
+        String phoneNumber,
+        Set<UserRole> userRoles
+) {
+    public static UserDTO fromEntity(User u){
+        return new UserDTO(
+                u.getId(),
+                u.getUsername(),
+                u.getFirstname(),
+                u.getLastname(),
+                u.getEmail(),
+                u.getPhoneNumber(),
+                u.getUserRoles()
+        );
+    }
+}
