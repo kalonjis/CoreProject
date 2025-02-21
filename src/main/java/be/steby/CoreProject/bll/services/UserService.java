475ff8c8b34cb4caa8c541b206c41665d1bd006c
kalonjis <kalonjistephane@gmail.com>
2025-02-21 14:33:19 +0100

    added new methods "getUserByEmail", "saveUser", "setUserEnabled", "setUserDisabled", "setUserMailVerified", "addRole"

--- src/main/java/be/steby/CoreProject/bll/services/UserService.java
+++ src/main/java/be/steby/CoreProject/bll/services/UserService.java
@@ -2,6 +2,7 @@ package be.steby.CoreProject.bll.services;
 
 
 import be.steby.CoreProject.dl.entities.User;
+import be.steby.CoreProject.dl.enums.UserRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,18 @@ public interface UserService {
     Page<User> searchUsersByCriteria(String username, String firstname, String lastname, String email, String phoneNumber, Pageable pageable);
 
     User getUserById(Long id);
+
+    User getUserByEmail(String email);
+
+    void saveUser(User user);
+
+    void setUserEnabled(User user);
+
+    void setUserDisabled(User user);
+
+    void setUserMailVerified(User user);
+
+    void addRole(User user, UserRole role);
+
 }
 
