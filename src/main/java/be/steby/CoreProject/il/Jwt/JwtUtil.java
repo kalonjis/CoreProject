34d36894a574ab53ffe2c9130a44cd723f5062e8
kalonjis <kalonjistephane@gmail.com>
2025-02-21 14:46:30 +0100

    deleted useless : constructor param "refreshTokenExpiration", refreshTokenIdCookieName and prints

--- src/main/java/be/steby/CoreProject/il/Jwt/JwtUtil.java
+++ src/main/java/be/steby/CoreProject/il/Jwt/JwtUtil.java
@@ -20,28 +20,19 @@ public class JwtUtil {
 
     private final SecretKey key;
     private final long accessTokenExpiration;
-    private final long refreshTokenExpiration;
     private final String accessTokenCookieName; // renommé de cookieName
     private final String refreshTokenCookieName;
-    private final String refreshTokenIdCookieName;
 
     public JwtUtil(
             @Value("${security.jwt.secret}") String secretKey,
             @Value("${security.jwt.access-token.expiration}") long accessTokenExpiration,
-            @Value("${security.jwt.refresh-token.expiration}") long refreshTokenExpiration,
             @Value("${security.jwt.access-token.name}") String accessTokenCookieName,
-            @Value("${security.jwt.refresh-token.name}") String refreshTokenCookieName) {
-
-        System.out.println("Secret Key: " + secretKey);
-        System.out.println("Access Expiration: " + accessTokenExpiration);
-        System.out.println("Refresh Expiration: " + refreshTokenExpiration);
-
+            @Value("${security.jwt.refresh-token.name}") String refreshTokenCookieName)
+    {
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
         this.accessTokenExpiration = accessTokenExpiration;
-        this.refreshTokenExpiration = refreshTokenExpiration;
         this.accessTokenCookieName = accessTokenCookieName;
         this.refreshTokenCookieName = refreshTokenCookieName;
-        this.refreshTokenIdCookieName = "refresh_token_id"; // valeur fixe
     }
 
     public String generateAccessToken(User user) {
