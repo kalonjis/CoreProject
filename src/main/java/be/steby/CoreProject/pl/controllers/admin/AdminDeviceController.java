f0bc766bc780276b8e976e3e48e176a07abc56ff
kalonjis <kalonjistephane@gmail.com>
2025-04-16 14:56:04 +0200

    activated endpoint "@GetMapping("/list/user/{id}")"

--- src/main/java/be/steby/CoreProject/pl/controllers/admin/AdminDeviceController.java
+++ src/main/java/be/steby/CoreProject/pl/controllers/admin/AdminDeviceController.java
@@ -1,15 +1,17 @@
 package be.steby.CoreProject.pl.controllers.admin;
 
 
+import be.steby.CoreProject.bll.services.AdminService;
 import be.steby.CoreProject.bll.services.DeviceService;
-import be.steby.CoreProject.dl.enums.DeviceTrustLevel;
-import be.steby.CoreProject.il.device.RequiresDeviceTrustLevel;
+import be.steby.CoreProject.dl.entities.User;
+import be.steby.CoreProject.pl.models.device.DeviceDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -22,14 +24,18 @@ import java.util.Map;
 public class AdminDeviceController {
 
     private final DeviceService deviceService;
+    private final AdminService adminService;
 
 
 
-//    @GetMapping("/device/list/user/{id}")
-//    public ResponseEntity<List<Device>> getUserDevices(@PathVariable Long id) {
-//        List<Device> devices = deviceService.getUserDevices(id);
-//        return ResponseEntity.ok(devices);
-//    }
+    @GetMapping("/list/user/{id}")
+    public ResponseEntity<List<DeviceDTO>> getUserDevices(@PathVariable Long id) {
+        User user = adminService.getUserById(id);
+        List<DeviceDTO> devicesDTO = deviceService.getUserDevice(user).stream()
+                .map(DeviceDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(devicesDTO);
+    }
 
 
     @GetMapping("/count-total")
