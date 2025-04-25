4999d15954ef6d9ac7c17b54a2ac5218ceea3731
kalonjis <kalonjistephane@gmail.com>
2025-04-25 21:06:40 +0200

    added "@EnableScheduling" on CoreProjectApplication

--- src/main/java/be/steby/CoreProject/CoreProjectApplication.java
+++ src/main/java/be/steby/CoreProject/CoreProjectApplication.java
@@ -3,9 +3,11 @@ package be.steby.CoreProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableScheduling
 public class CoreProjectApplication {
 
 	public static void main(String[] args) {
