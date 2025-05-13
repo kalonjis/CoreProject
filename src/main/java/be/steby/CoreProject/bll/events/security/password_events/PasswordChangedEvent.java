4b3fb67f3dd0e0277637d9d63f23b2a3ad237453
kalonjis <kalonjistephane@gmail.com>
2025-05-13 20:53:44 +0200

    update

--- src/main/java/be/steby/CoreProject/bll/events/security/password_events/PasswordChangedEvent.java
+++ src/main/java/be/steby/CoreProject/bll/events/security/password_events/PasswordChangedEvent.java
@@ -1,9 +1,11 @@
 package be.steby.CoreProject.bll.events.security.password_events;
 
 import be.steby.CoreProject.bll.models.RequestContext;
+import be.steby.CoreProject.dl.entities.Device;
 import be.steby.CoreProject.dl.entities.User;
 
 public record PasswordChangedEvent(
         User user,
+        Device device,
         RequestContext requestContext
     ) { }
