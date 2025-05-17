4b2fb09601faac24f9d7a78c73f0a62282b9b4f6
kalonjis <kalonjistephane@gmail.com>
2025-05-17 14:16:37 +0200

    update 2

--- src/main/java/be/steby/CoreProject/pl/security/validators/StrongPasswordValidator.java
+++ src/main/java/be/steby/CoreProject/pl/security/validators/StrongPasswordValidator.java
@@ -1,56 +1,33 @@
 package be.steby.CoreProject.pl.security.validators;
 
+import be.steby.CoreProject.bll.domain.password.models.PasswordValidationResult;
+import be.steby.CoreProject.bll.domain.password.services.PasswordPolicyService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class StrongPasswordValidator implements ConstraintValidator<StrongPassword, String> {
+    private final PasswordPolicyService policyService;
 
-    @Override
-    public void initialize(StrongPassword constraintAnnotation) {
-        // Rien à initialiser
-    }
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
-        // Désactiver le message par défaut
-        context.disableDefaultConstraintViolation();
-
-        // Vérifier si le mot de passe est null ou vide (autres annotations s'en occupent)
         if (password == null || password.isEmpty()) {
-            return true;
+            return true; // Let @NotNull/@NotBlank handle this
         }
 
-        boolean hasUpperCase = password.matches(".*[A-Z].*");
-        boolean hasLowerCase = password.matches(".*[a-z].*");
-        boolean hasDigit = password.matches(".*\\d.*");
-        boolean hasSpecialChar = password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*");
+        PasswordValidationResult result = policyService.validatePassword(password);
 
-        boolean isValid = true;
-
-        if (!hasUpperCase) {
-            context.buildConstraintViolationWithTemplate("Le mot de passe doit contenir au moins une lettre majuscule")
-                    .addConstraintViolation();
-            isValid = false;
+        if (!result.isValid()) {
+            // Add validation messages
+            context.disableDefaultConstraintViolation();
+            result.errors().forEach(error ->
+                    context.buildConstraintViolationWithTemplate(error)
+                            .addConstraintViolation()
+            );
         }
 
-        if (!hasLowerCase) {
-            context.buildConstraintViolationWithTemplate("Le mot de passe doit contenir au moins une lettre minuscule")
-                    .addConstraintViolation();
-            isValid = false;
-        }
-
-        if (!hasDigit) {
-            context.buildConstraintViolationWithTemplate("Le mot de passe doit contenir au moins un chiffre")
-                    .addConstraintViolation();
-            isValid = false;
-        }
-
-        if (!hasSpecialChar) {
-            context.buildConstraintViolationWithTemplate("Le mot de passe doit contenir au moins un caractère spécial")
-                    .addConstraintViolation();
-            isValid = false;
-        }
-
-        return isValid;
+        return result.isValid();
     }
 }
