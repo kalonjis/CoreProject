7759be43f856fca52da7eb1f7e5a1ef54edafb0d
kalonjis <kalonjistephane@gmail.com>
2025-09-02 10:54:15 +0200

    removed "@ResponseStatus(HttpStatus...") causing troubles with Advisor while handling errors

--- src/main/java/be/steby/CoreProject/bll/domains/auth/exceptions/AccountDisabledException.java
+++ src/main/java/be/steby/CoreProject/bll/domains/auth/exceptions/AccountDisabledException.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Exception thrown when a user account has been disabled by an administrator.
  * This should result in a 403 Forbidden response.
  */
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class AccountDisabledException extends AuthenticationException {
 
     public AccountDisabledException(String message) {
-        super(message);
+        super(message, 403);
     }
 }
