244a6934ad01829f957f126f4c1eb6ba51cf639a
kalonjis <kalonjistephane@gmail.com>
2025-09-30 10:40:47 +0200

    commented useless class

--- src/main/java/be/steby/CoreProject/il/configs/AspectConfig.java
+++ src/main/java/be/steby/CoreProject/il/configs/AspectConfig.java
@@ -1,9 +1,9 @@
-package be.steby.CoreProject.il.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy
-public class AspectConfig {
-}
+//package be.steby.CoreProject.il.configs;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//
+//@Configuration
+//@EnableAspectJAutoProxy
+//public class AspectConfig {
+//}
