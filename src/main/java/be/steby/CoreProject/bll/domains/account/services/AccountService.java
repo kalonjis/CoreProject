760fe324a7aeccfe7554494b12f312a781c8e6c0
kalonjis <kalonjistephane@gmail.com>
2025-10-03 10:32:15 +0200

    added new method : "User signup(SelfSignupRequest request, HttpServletRequest httpRequest);"

--- src/main/java/be/steby/CoreProject/bll/domains/account/services/AccountService.java
+++ src/main/java/be/steby/CoreProject/bll/domains/account/services/AccountService.java
@@ -2,10 +2,14 @@ package be.steby.CoreProject.bll.domains.account.services;
 
 import be.steby.CoreProject.bll.domains.account.models.DeactivationRequest;
 import be.steby.CoreProject.bll.domains.account.models.ReactivationRequest;
+import be.steby.CoreProject.bll.domains.account.models.SelfSignupRequest;
 import be.steby.CoreProject.dl.entities.User;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AccountService {
+
+    User signup(SelfSignupRequest request, HttpServletRequest httpRequest);
+
     User confirmNewUserAccount(String token, HttpServletRequest request);
 
     void resendActivation(String token, HttpServletRequest request);
