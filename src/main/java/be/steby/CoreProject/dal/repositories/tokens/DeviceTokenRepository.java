e1477b2a986f317884e1a066852dc524fde36737
kalonjis <kalonjistephane@gmail.com>
2026-01-25 14:10:26 +0100

    added "findAllByUserIdAndDeviceId" + "revokeAllByUserAndDevice" methods

--- src/main/java/be/steby/CoreProject/dal/repositories/tokens/DeviceTokenRepository.java
+++ src/main/java/be/steby/CoreProject/dal/repositories/tokens/DeviceTokenRepository.java
@@ -4,8 +4,10 @@ import be.steby.CoreProject.dl.entities.User;
 import be.steby.CoreProject.dl.entities.tokens.DeviceConfirmationToken;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface DeviceTokenRepository extends BaseTokenRepository<DeviceConfirmationToken> {
@@ -15,6 +17,13 @@ public interface DeviceTokenRepository extends BaseTokenRepository<DeviceConfirm
     Optional<DeviceConfirmationToken> findByIdAndRevokedFalse(Long id);
 
     Optional<DeviceConfirmationToken> findByToken(String token);
+
+    List<DeviceConfirmationToken> findAllByUserIdAndDeviceId(Long userId, Long deviceId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE DeviceConfirmationToken t SET t.revoked = true " +
+            "WHERE t.user.id = :userId AND t.deviceId = :deviceId AND t.revoked = false")
+    int revokeAllByUserAndDevice(@Param("userId") Long userId, @Param("deviceId") Long deviceId);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("DELETE FROM DeviceConfirmationToken t WHERE t.expiryDate < ?1 OR t.revoked = true")
